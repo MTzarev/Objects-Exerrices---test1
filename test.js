ccb9a424389807e20f3b5c6bdd900b8184a5f0d4
Miroslav Tsarev <mtzarev@gmail.com>
2024-04-19 17:52:23 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,9 @@
+let student = {
+    fistName: `Miro`,
+    lastName: `Tzarev`,
+    age: 33,
+    locacion:{town: `Plovdiv`, kvartal: `Smirnenski`}
+   
+}
+console.log(student);
+console.log(student.locacion.town);
